7ee8d1b3325d3be16db9274e89924710c48314a3
Timothee Deve <timdeve@users.noreply.github.com>
2016-06-19 21:32:01 +0100

    Split tmp and tmux config

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,3 +1,3 @@
 mv ~/.tmux.conf ~/.tmux.conf.bak
-ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/tmux/tmp.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
