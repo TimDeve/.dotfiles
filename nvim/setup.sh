43f99dc07ff1cd8369ea542923d4314b5f232ddc
Timothee Deve <timdeve@users.noreply.github.com>
2017-01-02 19:43:22 +0100

    No but really this time

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/.config/nvim
 mkdir ~/.config/nvim/undo
 mkdir ~/.config/nvim/autoload
-mv ~/.dotfiles/nvim/nvimrc ~/.dotfiles/nvim/nvimrc.bak
+mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak 
 ln -s ~/.dotfiles/nvim/nvimrc ~/.config/nvim/init.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
