b89019973b2b0ff6b9ea1dbdffc875d01df5f2b8
Timothee Deve <timdeve@users.noreply.github.com>
2017-01-02 19:40:26 +0100

    Fixes nvim setup.sh

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 mkdir -p ~/.config/nvim
+mkdir ~/.config/nvim/undo
+mkdir ~/.config/nvim/autoload
+mv ~/.dotfiles/nvim/nvimrc ~/.dotfiles/nvim/nvimrc.bak
 ln -s ~/.dotfiles/nvim/nvimrc ~/.config/nvim/init.vim
- ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+curl -fLo ~/.config/nvim/autoload/plug.vim \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir -p ~/.config/nvim/bundle
