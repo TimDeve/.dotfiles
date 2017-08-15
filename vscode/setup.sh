21219d6ace776b846629e6d331d0e42a465814be
Tim Dévé <timdeve@users.noreply.github.com>
2017-08-15 17:27:20 +0100

    Adds VS Code config files

---
+++ vscode/setup.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+ln -sf ~/.dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -sf ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
