7465c06780eb618b0febcc36c4cbe1fd143d84ed
Tim Dévé <TimDeve@users.noreply.github.com>
2023-01-08 15:24:06 +0000

    Removes folder that are not used

--- vscode/setup.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-ln -sf ~/.dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -sf ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
